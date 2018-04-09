123fa11c342f552d3b7a3402797f7fcd67600aa1
jfair <joe@fairanswers.com>
2018-04-09 19:22:58 -0400

    Added Say and Sing

--- fair_bpm.py
+++ fair_bpm.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python
 
-#import utest
+import utest
 
-# class Pretty(object):
-#     def __repr__(i):
-#         return i.__class__.__name__ + kv(i.__dict__)
-#
-# def kv(d):
-#     return '(' + ', '.join(['%s: %s' % (k, d[k])
-#                               for k in sorted(d.keys())
-#                               if k[0] != "_"]) + ')'
-#
-# class O(Pretty):
-#   def __init__(i, **adds): i.__dict__.update(adds)
-#
-#
+class Pretty(object):
+    def __repr__(i):
+        return i.__class__.__name__ + kv(i.__dict__)
 
-class Activity(object):
+def kv(d):
+    return '(' + ', '.join(['%s: %s' % (k, d[k])
+                              for k in sorted(d.keys())
+                              if k[0] != "_"]) + ')'
+
+class O(Pretty):
+  def __init__(i, **adds): i.__dict__.update(adds)
+
+
+
+class Activity(Pretty):
     def __init__(self, id, name):
         print("Init Activity")
         def __init__(self, id=-1, name='unknown'):
@@ -27,13 +27,25 @@ class Activity(object):
     def execute(self):
         print("In Activity Execute. id="+self.id+" name="+self.name)
 
+class Say(Activity):
+    def execute(self):
+        print("In Say")
+
+
+class Sing(Activity):
+    def execute(self):
+        print("In Sing")
 
 print "SFSG"
-a=Activity("id11", "name22")
-print a.id
-print a.name
-a.execute()
+one=Activity("id11", "name22")
+print one.id
+print one.name
+one.execute()
+
+two=Say("id22", "The Say Activity")
+three=Sing("id33", "The Sing Activity")
 
 
-
+two.execute()
+three.execute()
 
