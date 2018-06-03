3cba0adbf98754c3222cf2ba3d274da442b0dbc8
joe.fair <joe@bodkinconsulting.com>
2018-06-03 15:32:37 -0400

    Got delete working.  Need 'run'

--- front_end.py
+++ front_end.py
@@ -1,18 +1,30 @@
 #!/usr/bin/python
 from flask import Flask, render_template, request
-from fair_bpm import file_dot_data_store
+from fair_bpm import file_dot_data_store, Process
 app = Flask(__name__)
 store = file_dot_data_store()
 
 @app.route('/')
-def hello():
+def index():
     mylist=store.list()
     return render_template('index.html', list=mylist)
+
+@app.route('/delete/<string:id>/', methods = ['DELETE'])
+def dot_delete(id):
+    store.delete(id)
+    return "Deleted."
 
 @app.route("/show/<string:id>/", methods = ['GET', 'POST', 'DELETE'])
 def show(id):
     ps=store.load(id)
     return render_template('show.html', id=id, ps=ps)
+
+@app.route("/save/<string:id>/", methods = ['GET', 'POST', 'DELETE'])
+def save(id):
+    file = request.data
+    ps = Process.parse(file)
+    store.save(ps)
+    return ps.to_dot()
 
 if __name__ == '__main__':
     print("Starting")
